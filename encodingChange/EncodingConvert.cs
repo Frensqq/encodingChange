a2ec60e733c00e2a5a1bf2146b95292198e1a614
Frensqq <liadovn@yandex.ru>
2026-04-01 13:51:38 +0300

    refactoring encoding methods

--- encodingChange/EncodingConvert.cs
+++ encodingChange/EncodingConvert.cs
@@ -18,39 +18,38 @@ namespace encodingChange
             return Encoding.GetEncoding(to).GetString(byteStr);
         }
 
+        //Utf-8 в другие форматы
         public static string Utf8ToWindows1251(string input)
-        {
-            return Convert(input, "utf-8", "windows-1251");
-        }
-
-        public static string Windows1251ToUtf8(string input)
-        {
-            return Convert(input, "windows-1251", "utf-8");
-        }
-
+            => Convert(input, "utf-8", "windows-1251");
         public static string Utf8ToAscii(string input)
-        {
-            return Convert(input, "utf-8", "ascii");
-        }
+            => Convert(input, "utf-8", "ascii");
+        public static string Utf8ToKoi8r(string input)
+            => Convert(input, "utf-8", "koi8-r");
+
+
+        //Windows1251 в другие форматы
+        public static string Windows1251ToUtf8(string input) 
+            => Convert(input, "windows-1251", "utf-8");
+        public static string Windows1251ToAscii(string input)
+            => Convert(input, "windows-1251", "ascii");
+        public static string Windows1251ToKoi8r(string input)
+            => Convert(input, "windows-1251", "koi8-r");
+
+        //Ascii в другие форматы
         public static string AsciiToUtf8(string input)
-        {
-            return Convert(input, "ascii", "utf-8");
-        }
+            => Convert(input, "ascii", "utf-8");
 
-        public static string AsciiToWindows1251(string input)
-        {
-            return Convert(input, "ascii", "windows-1251");
-        }
+        public static string AsciiToWindows1251(string input) 
+            => Convert(input, "ascii", "windows-1251");
+        public static string AsciiToKoi8r(string input)
+            => Convert(input, "ascii", "koi8-r");
 
-        public static string Windows1251ToAscii(string input) 
-        {
-            return Convert(input, "windows-1251", "ascii");
-        }
-        public static string Utf8ToKoi8r(string input) => Convert(input, "utf-8", "koi8-r");
-        public static string Koi8rToUtf8(string input) => Convert(input, "koi8-r", "utf-8");
-        public static string Windows1251ToKoi8r(string input) => Convert(input, "windows-1251", "koi8-r");
-        public static string Koi8rToWindows1251(string input) => Convert(input, "koi8-r", "windows-1251");
-        public static string AsciiToKoi8r(string input) => Convert(input, "ascii", "koi8-r");
-        public static string Koi8rToAscii(string input) => Convert(input, "koi8-r", "ascii");
+        //Koi8-R в другие форматы
+        public static string Koi8rToUtf8(string input) 
+            => Convert(input, "koi8-r", "utf-8");
+        public static string Koi8rToWindows1251(string input) 
+            => Convert(input, "koi8-r", "windows-1251");
+        public static string Koi8rToAscii(string input) 
+            => Convert(input, "koi8-r", "ascii");
     }
 }
