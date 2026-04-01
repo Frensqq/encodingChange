23cb31030764cc93a4518d2bad4caa9253d8645a
Frensqq <liadovn@yandex.ru>
2026-04-01 13:24:07 +0300

    create universal function

---
+++ encodingChange/EncodingConvert.cs
@@ -0,0 +1,21 @@
+﻿ using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace encodingChange
+{
+    public class EncodingConvert
+    {
+        //Универсальный метод для создания публичных
+        private static string Convert(string input, string from , string to)
+        {
+            if (input == null) throw new ArgumentNullException("input");
+            if (input == "") return "";
+
+            byte[] byteStr = Encoding.GetEncoding(from).GetBytes(input);
+            return Encoding.GetEncoding(to).GetString(byteStr);
+        }
+    }
+}
