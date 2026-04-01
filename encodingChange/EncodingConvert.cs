2444c2b96078f62c530ae13f0fb9e94556b3cca7
Frensqq <liadovn@yandex.ru>
2026-04-01 13:29:12 +0300

    create universal function

--- encodingChange/EncodingConvert.cs
+++ encodingChange/EncodingConvert.cs
@@ -9,13 +9,42 @@ namespace encodingChange
     public class EncodingConvert
     {
         //Универсальный метод для создания публичных
-        private static string Convert(string input, string from , string to)
+        private static string Convert(string input, string from, string to)
         {
             if (input == null) throw new ArgumentNullException("input");
             if (input == "") return "";
 
             byte[] byteStr = Encoding.GetEncoding(from).GetBytes(input);
             return Encoding.GetEncoding(to).GetString(byteStr);
+        }
+
+        public static string Utf8ToWindows1251(string input)
+        {
+            return Convert(input, "utf-8", "windows-1251");
+        }
+
+        public static string Windows1251ToUtf8(string input)
+        {
+            return Convert(input, "windows-1251", "utf-8");
+        }
+
+        public static string Utf8ToAscii(string input)
+        {
+            return Convert(input, "utf-8", "ascii");
+        }
+        public static string AsciiToUtf8(string input)
+        {
+            return Convert(input, "ascii", "utf-8");
+        }
+
+        public static string AsciiToWindows1251(string input)
+        {
+            return Convert(input, "ascii", "windows-1251");
+        }
+
+        public static string Windows1251ToAscii(string input) 
+        {
+            return Convert(input, "windows-1251", "ascii");
         }
     }
 }
